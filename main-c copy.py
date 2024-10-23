98bf7ee5cffac4d9f47fc6a77304a3d0e79d2012
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-10-23 16:21:23 +0900

    deploy

--- main-c copy.py
+++ main-c copy.py
@@ -71,34 +71,9 @@ class MainC:
     def prepare_firestore(self):
         try:
             if not firebase_admin._apps:
-                type = st.secrets["type"]
-                project_id = st.secrets["project_id"]
-                private_key_id = st.secrets["private_key_id"]
-                private_key = st.secrets["private_key"].replace("\\n", "\n")
-                client_email = st.secrets["client_email"]
-                client_id = st.secrets["client_id"]
-                auth_uri = st.secrets["auth_uri"]
-                token_uri = st.secrets["token_uri"]
-                auth_provider_x509_cert_url = st.secrets["auth_provider_x509_cert_url"]
-                client_x509_cert_url = st.secrets["client_x509_cert_url"]
-                universe_domain = st.secrets["universe_domain"]
-                # Firebase認証情報を設定
-                cred = credentials.Certificate(
-                    {
-                        "type": type,
-                        "project_id": project_id,
-                        "private_key_id": private_key_id,
-                        "private_key": private_key,
-                        "client_email": client_email,
-                        "client_id": client_id,
-                        "auth_uri": auth_uri,
-                        "token_uri": token_uri,
-                        "auth_provider_x509_cert_url": auth_provider_x509_cert_url,
-                        "client_x509_cert_url": client_x509_cert_url,
-                        "universe_domain": universe_domain,
-                    }
-                )
-                default_app = firebase_admin.initialize_app(cred)
+                JSON_PATH = st.secrets["FIREBASE_JSON"]
+                cred = credentials.Certificate(JSON_PATH)
+                firebase_admin.initialize_app(cred)
             db = firestore.client()
             return db
 
@@ -195,39 +170,38 @@ class MainC:
                 )
                 self.disable_chat_input()
 
-            else:
-                if user_input := st.chat_input(
-                    "メッセージを送る",
-                    disabled=st.session_state.chat_input_disabled,
-                    on_submit=self.disable_chat_input(),
-                ):
-                    # チャットメッセージコンテナにユーザーメッセージを表示
-                    st.chat_message("user").markdown(user_input)
+            if user_input := st.chat_input(
+                "メッセージを送る",
+                disabled=st.session_state.chat_input_disabled,
+                on_submit=self.disable_chat_input(),
+            ):
+                # チャットメッセージコンテナにユーザーメッセージを表示
+                st.chat_message("user").markdown(user_input)
 
-                    # チャット履歴にユーザーメッセージを追加
-                    st.session_state.message_history.append(
-                        {"role": "user", "content": user_input}
+                # チャット履歴にユーザーメッセージを追加
+                st.session_state.message_history.append(
+                    {"role": "user", "content": user_input}
+                )
+
+                with st.spinner("Wait for it..."):
+                    # AIからの応答を取得、データベースに登録
+                    assistant_response = self.generate_and_store_response(
+                        user_input, st.session_state.db
                     )
 
-                    with st.spinner("Wait for it..."):
-                        # AIからの応答を取得、データベースに登録
-                        assistant_response = self.generate_and_store_response(
-                            user_input, st.session_state.db
-                        )
+                # AIからの応答を表示
+                with st.chat_message("assistant"):
+                    st.markdown(assistant_response)
 
-                    # AIからの応答を表示
-                    with st.chat_message("assistant"):
-                        st.markdown(assistant_response)
+                # チャット履歴にAIからの応答を追加
+                st.session_state.message_history.append(
+                    {"role": "assistant", "content": assistant_response}
+                )
 
-                    # チャット履歴にAIからの応答を追加
-                    st.session_state.message_history.append(
-                        {"role": "assistant", "content": assistant_response}
-                    )
+                st.session_state.count += 1
 
-                    st.session_state.count += 1
-
-                    self.enable_chat_input()
-                    st.rerun()
+                self.enable_chat_input()
+                st.rerun()
 
 
 if __name__ == "__main__":
