b0d51b5f84ae158999f3aa965883e52041a50b2a
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-10-24 20:32:01 +0900

    Update main-r.py

--- main-r.py
+++ main-r.py
@@ -63,9 +63,7 @@ class MainR:
         self.SYSTEM_PREFIX = (
             "You are an assistant for question-answering tasks. "
             "Use the following pieces of retrieved context to answer "
-            "the question. If you don't know the answer, say that you "
-            "don't know. Use three sentences maximum and keep the "
-            "answer concise."
+            "the question. Use three sentences maximum and keep the answer concise."
             "\n\n"
             "{context}"
         )
