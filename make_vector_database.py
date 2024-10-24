4d79c0415ae9c15ad41ba766bdbee5b5741970f7
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-10-24 16:01:07 +0900

    Update vector_database

--- make_vector_database.py
+++ make_vector_database.py
@@ -11,7 +11,7 @@ CHROMA_DB_PATH = "vector_database"
 def embed_data():
     loader = CSVLoader(file_path="liquid_neural_network.csv", encoding="utf-8")
     documents = loader.load()
-    text_splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0)
+    text_splitter = CharacterTextSplitter(chunk_size=800, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
     embed = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
     vector_db = Chroma.from_documents(texts, embed, persist_directory=CHROMA_DB_PATH)
