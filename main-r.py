2df1bceeb1ae1f4818e32550e051918d8fe75935
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-10-24 22:23:34 +0900

    the Garden of sinners

--- main-r.py
+++ main-r.py
@@ -44,7 +44,7 @@ class MainR:
             "without the chat history. Do NOT answer the question, "
             "just reformulate it if needed and otherwise return it as is."
         )
-        self.CHROMA_DB_PATH = "vector_database/diary"
+        self.CHROMA_DB_PATH = "vector_database/the_Garden_of_sinners"
         self.embed = OpenAIEmbeddings(openai_api_key=st.secrets["OPENAI_API_KEY"])
         self.vector_db = Chroma(
             persist_directory=self.CHROMA_DB_PATH, embedding_function=self.embed
--- make_vector_database.py
+++ make_vector_database.py
@@ -4,12 +4,13 @@ from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.vectorstores import Chroma
 import streamlit as st
 
+name = "the_Garden_of_sinners"
 
-CHROMA_DB_PATH = "vector_database/diary"
+CHROMA_DB_PATH = f"vector_database/{name}"
 
 
 def embed_data():
-    loader = CSVLoader(file_path="diary.csv", encoding="utf-8")
+    loader = CSVLoader(file_path=f"{name}.csv", encoding="utf-8")
     documents = loader.load()
     text_splitter = CharacterTextSplitter(chunk_size=800, chunk_overlap=0)
     texts = text_splitter.split_documents(documents)
