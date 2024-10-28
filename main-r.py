9e7f316b7f0bf8f9aabf463b854e6eca2b07a161
Yoshimuraeto <118145058+Yoshimuraeto@users.noreply.github.com>
2024-10-28 23:59:01 +0900

    Update main-r.py

--- main-r.py
+++ main-r.py
@@ -129,10 +129,26 @@ class MainR:
     def prepare_model_with_memory(self):
         if "chat_history" not in st.session_state:
             st.session_state.chat_history = []
-        vector_db = Chroma(
-            persist_directory=self.CHROMA_DB_PATH, embedding_function=self.embed
-        )
-        retriever = vector_db.as_retriever()
+            name = "the_Garden_of_sinners"
+            CHROMA_DB_PATH = f"vector_database/{name}"
+            from langchain.document_loaders import TextLoader
+            from langchain.text_splitter import CharacterTextSplitter
+
+            loader = TextLoader(f"{name}.txt", encoding="utf-8")
+            data = loader.load()
+            text_splitter = CharacterTextSplitter(
+                separator="\n\n", chunk_size=900, chunk_overlap=0, length_function=len
+            )
+            documents = text_splitter.create_documents(
+                [doc.page_content for doc in data]
+            )
+            st.session_state.vector_db = Chroma.from_documents(
+                documents=documents,
+                embedding=self.embed,
+                persist_directory=CHROMA_DB_PATH,
+            )
+            st.write("vector_db", st.session_state.vector_db._collection.count())
+        retriever = st.session_state.vector_db.as_retriever()
         st.session_state.history_aware_retriever = create_history_aware_retriever(
             self.chat_model, retriever, self.CONTEXTUALIZE_Q_PROMPT
         )
